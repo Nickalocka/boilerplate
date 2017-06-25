754855754223365764f4ef69645d32021027df13
Nickalocka <nickystrife@gmail.com>
2017-06-25 18:30:24 +0100

    routing added, themeing started, autoprefixer added

--- _scripts/_app.js
+++ _scripts/_app.js
@@ -1,11 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
     app.Init();
 });
 
 var app = (function (_module) {
 
     _module.Init = function () {
-        console.log("doc ready");
+        route();
+    };
+
+    var route = function () {
+
+        $(window).on('hashchange', function (e) {
+            var page = "home";
+
+            if (window.location.hash) {
+                page = window.location.hash.replace("#", "");
+            }
+
+            loadPage(page);
+
+        });
+
+        if (window.location.hash) {
+            $(window).trigger('hashchange')
+        } else {
+            loadPage("home");
+        };
+    };
+
+    var loadPage = function (page) {
+        var pageURL = "/_pages/_" + page + ".html";
+
+        $.ajax({
+            url: pageURL
+        }).done(function (html) {
+            $('[data-content="main"]').html(html);
+        }).fail(function (resp) {
+            loadPage("404");
+        });
     };
 
     return _module;
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     gulp.src('_styles/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./_dist/assets/css'));
+});
+
+gulp.task('autoprefixer', function () {
+    gulp.src('./_dist/assets/css/styles.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./_dist/assets/css'))
 });
 
 var files = [
@@ -13,15 +23,16 @@ var files = [
     '_scripts/_app.js',
     '_scripts/**/_*.js'
 ];
- 
-gulp.task('scripts', function() {
-  return gulp.src(files)
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./_dist/assets/js'));
+
+gulp.task('scripts', function () {
+    return gulp.src(files)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./_dist/assets/js'));
 });
 
 gulp.task('watch', function () {
     gulp.watch('_styles/**/*.scss', ['styles']);
+    gulp.watch('./_dist/assets/css/styles.css', ['autoprefixer']);
     gulp.watch('_scripts/**/_*.js', ['scripts']);
 });
 
